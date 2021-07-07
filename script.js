e3814027a73879a02521022f38853c2424f5a5d2
Fernando X. Avila-Soto <fxavilasoto@gmail.com>
2021-07-07 15:35:55 -0400

    Updated script.js
    Added variables and function to compute interest rate

--- script.js
+++ script.js
@@ -2,5 +2,24 @@ function compute()
 {
     p = document.getElementById("principal").value;
     
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    // Calculate interest rate
+    var interest = principal * years * rate / 100;
+
+    // Convert no. of years into actual year in the future
+    var year = new Date().getFullYear()+parseInt(years);
 }
-        
+
+// Read the value of the range slider and display it in the <span> tag adjacent to the slider
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+<select onchange="updateRate()">
+
+</select>
